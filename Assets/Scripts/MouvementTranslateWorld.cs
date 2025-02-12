4242d4ee6b77278c0fca39099075843fd32cb097
KorinnaG <korinna.gladhill@gmail.com>
2025-02-12 16:47:43 -0500

    Le poisson avance par rapport a comment on le met dans linspecteur

--- Assets/Scripts/MouvementTranslateWorld.cs
+++ Assets/Scripts/MouvementTranslateWorld.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MouvementTranslateWorld : MonoBehaviour
-{
+{        [SerializeField] private Vector3 _deplacement;
+
     void Update()
     {
-        transform.Translate(1f * Time.deltaTime, 0, 0, Space.World);
+        Vector3 mouvement = _deplacement * Time.deltaTime;
+        transform.Translate(mouvement, Space.Self);
 
         //Vector3 deplacement = new Vector3(1f, 0, 0);
         //transform.Translate(deplacement * Time.deltaTime, Space.World);
